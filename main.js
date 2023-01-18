26ad6ea764fce0ed222076050db4ef65ae194bf5
monaSalih <monaalobisat@gmail.com>
2023-01-18 21:10:43 +0300

    tt

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+if (1<19){
+    let d=3;
+    console.log(d);
+}
+console.log(d);
+
+//how write if condition compare between 2 number?
+function myFunction() {
+    var answer = parseInt(document.getElementById('answer').value,10);
+    if (answer === 10)
+        document.getElementById("valid").innerHTML = "Correct!";
+    else
+        document.getElementById("valid").innerHTML = "Please, Try Again!";
+}
+
+
+
+
+//Source: https://stackoverflow.com/questions/26124345
+
+
